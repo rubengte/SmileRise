46724c0e9c42cf74c6a6113ac8d6ef0a79603892
Ruben GT <rubengt2007@gmail.com>
2025-07-13 22:03:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -108,7 +108,8 @@ function App() {
           </div>
           <p className="text-gray-600 text-lg max-w-2xl mx-auto">
             Select a video (up to 10GB) and automatically extract genuine smiling faces using{' '}
-            <strong>a hybrid technology approach, combining Face-API.js machine learning with computer vision geometric analysis</strong>. <br />
+            <strong>a hybrid technology approach,</strong>. <br />
+             combining Face-API.js machine learning with computer vision geometric analysis.
           </p>
         </div>
 
